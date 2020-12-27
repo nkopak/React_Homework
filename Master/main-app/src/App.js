07a5c06d1efc0544ce0c9b8ad6ee03cc01996a45
nazariikopak <123nazik@gmail.com>
2020-12-27 02:02:59 +0200

    react_redux_hw_2

--- Master/main-app/src/App.js
+++ Master/main-app/src/App.js
@@ -1,43 +1,12 @@
 import React, {useState, useEffect, useReducer} from 'react';
+import {useSelector,useDispatch} from "react-redux";
 
-
-let test = 0;
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'SET_TODO': {
-            return action.payload
-        }
-        case 'CHANGE_TODO_STATUS': {
-            return {
-                ...state,
-                completed: !state.completed
-            };
-        }
-        case 'CHANGE_TODO_TITLE': {
-            return {
-                ...state,
-                title: action.payload
-            };
-        }
-        default: {
-            console.error('Didnt found case for action:', action)
-            return state;
-        }
-    }
-}
-
-const initialState = {
-    userId: null,
-    id: null,
-    title: "",
-    completed: false
-}
 
 export default function App() {
-    const [state,dispatch] = useReducer(reducer,initialState);
 
     const [counter, setCounter] = useState(1);
+    const todo = useSelector((todo)=>todo);
+    const dispatch = useDispatch();
 
     useEffect(() => {
         fetch(`https://jsonplaceholder.typicode.com/todos/${counter}`)
@@ -48,24 +17,17 @@ export default function App() {
             })
     }, [counter])
 
-    const onCLickIncrease = () => setCounter((prevState) => prevState + 1);
-
-    const onChangeStatus = () => dispatch({type: 'CHANGE_TODO_STATUS'});
-    const onChangeTitle = () => dispatch({type: 'CHANGE_TODO_TITLE', payload: Math.random()});
-
     return (
         <div>
-            {/*{user.map(el=><h2>{el}</h2>)}*/}
-            <button onClick={onCLickIncrease}>Increase count</button>
-            <button onClick={onChangeStatus}>Change status</button>
-            <button onClick={onChangeTitle}>Change title</button>
+            <button onClick={() => setCounter((prevState) => prevState + 1)}>Increase count</button>
+            <button onClick={() => dispatch({type: 'CHANGE_TODO_STATUS'})}>Change status</button>
+            <button onClick={() => dispatch({type: 'CHANGE_TODO_TITLE', payload: Math.random()})}>Change title</button>
             <h1>Counter: {counter}</h1>
-            {/*<h1>{test}</h1>*/}
-            {!!state &&
+            {!!todo &&
             (<div>
-                <h1>Id: {state.id}</h1>
-                <h1>Title: {state.title}</h1>
-                <h1>Completed: {state.completed.toString()}</h1>
+                <h1>Id: {todo.id}</h1>
+                <h1>Title: {todo.title}</h1>
+                <h1>Completed: {todo.completed.toString()}</h1>
             </div>)}
         </div>
     );
--- Master/main-app/src/index.js
+++ Master/main-app/src/index.js
@@ -3,12 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+
+const initialState = {
+    userId: null,
+    id: null,
+    title: "",
+    completed: false
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_TODO': {
+            return action.payload
+        }
+        case 'CHANGE_TODO_STATUS': {
+            return {
+                ...state,
+                completed: !state.completed
+            };
+        }
+        case 'CHANGE_TODO_TITLE': {
+            return {
+                ...state,
+                title: action.payload
+            };
+        }
+        default: {
+            console.error('Didnt found case for action:', action)
+            return state;
+        }
+    }
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
